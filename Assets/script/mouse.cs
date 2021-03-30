3d14e9c3046334f5350199e827b31c2519e5d571
cyanxiao <3122962374@qq.com>
2021-03-30 22:59:44 +0800

    Initial commit

---
+++ Assets/script/mouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse : MonoBehaviour
+{
+    private float mousey;
+    private float mousex;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousex += Input.GetAxis("Mouse X");
+        mousey += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.AngleAxis(mousex, Vector3.up) * Quaternion.AngleAxis(mousey, Vector3.left);
+    }
+}
