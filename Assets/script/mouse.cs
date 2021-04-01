f5d4cb85480639cf9a4c638e40084df5d7cce9b3
cyanxiao <3122962374@qq.com>
2021-04-01 12:09:15 +0800

    更新鼠标控制旋转功能

--- Assets/script/mouse.cs
+++ Assets/script/mouse.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class mouse : MonoBehaviour
 {
+    public Transform target;
     private float mousey;
     private float mousex;
     // Start is called before the first frame update
@@ -18,5 +19,15 @@ public class mouse : MonoBehaviour
         mousex += Input.GetAxis("Mouse X");
         mousey += Input.GetAxis("Mouse Y");
         transform.localRotation = Quaternion.AngleAxis(mousex, Vector3.up) * Quaternion.AngleAxis(mousey, Vector3.left);
+        if (Input.GetKey(KeyCode.Mouse1))
+        {
+            var mouse_x = Input.GetAxis("Mouse X");
+            var mouse_y = -Input.GetAxis("Mouse Y");
+            transform.LookAt(target);
+            transform.RotateAround(target.transform.position, Vector3.up, mouse_x * 5);
+            transform.RotateAround(target.transform.position, transform.right, mouse_y * 5);
+            mousex = this.transform.localEulerAngles.y;
+            mousey = -this.transform.localEulerAngles.x;
+        }
     }
 }
